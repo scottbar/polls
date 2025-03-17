f550047f441d84ba6aadbcbbd1b4ee37e46ef33f
ScottB <velocharts@users.noreply.github.com>
2025-03-17 17:55:24 +0000

    Create approval.py

---
+++ approval.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import requests
+
+# Load data from the API response
+url = "https://www.politico.eu/wp-json/politico/v1/poll-of-polls/GB-leadership-approval"
+data = requests.get(url).json()
+
+# Extract relevant data
+polls = pd.DataFrame(data['polls'])
+trends = pd.DataFrame(data['trends']['kalmanSmooth'])
+
+# Combine trends data
+trends_parties = pd.DataFrame(trends['parties'].tolist())
+full_trends = pd.concat([trends, trends_parties], axis=1).drop(columns=['parties'])
+full_trends.tail(
